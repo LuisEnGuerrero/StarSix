17806fa401afffd11a277288736707569fd174c0
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-12-02 02:15:28 -0500

    Fix index.html from server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/starsix-educational-app'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/starsix-educational-app/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: path.join(__dirname+'/dist/starsix-educational-app/')});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
