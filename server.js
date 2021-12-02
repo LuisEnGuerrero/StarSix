d4d304f38efdaed2542956ce6eeb6efc9304aaaf
Luis Enrique Guerrero <luisenguerrero@yahoo.com>
2021-12-02 10:06:18 -0500

    RUN: ng build and upload server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/starsix-educational-app'));
+app.use(express.static(__dirname + '/dist/StarSixProyect'));
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname+'/dist/starsix-educational-app/')});
+    res.sendFile('index.html', {root: path.join(__dirname+'/dist/StarSixProyect/')});
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
